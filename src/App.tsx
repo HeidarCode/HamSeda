078a42d8c0a6d21271aa8a9a03bcf2a4f3f5799b
mohammadhosseinheidary <ho33ein.2003h@gmail.com>
2026-01-06 09:05:30 -0800

    fix router and vite base for github pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import VoiceActorsPage from './VoiceActorsPage';
-import AdminPanel from './AdminPanel';
-import AuthPage from './AuthPage';
+import { HashRouter, Routes, Route } from "react-router-dom";
+import VoiceActorsPage from "./VoiceActorsPage";
+import AdminPanel from "./AdminPanel";
+import AuthPage from "./AuthPage";
 
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <BrowserRouter>
-
-      {/* هدر باید داخل BrowserRouter باشد */}
-  
-    {/* <VoiceActorsPage /> */}
+    <HashRouter>
       <Routes>
-         <Route path="/" element={<VoiceActorsPage />} />
+        <Route path="/" element={<VoiceActorsPage />} />
         <Route path="/auth" element={<AuthPage />} />
         <Route path="/admin" element={<AdminPanel />} />
       </Routes>
-
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
